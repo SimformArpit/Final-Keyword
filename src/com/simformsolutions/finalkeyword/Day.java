3a74e197d4b135a4bd2e884846bc789e2eceb584
SimformArpit <arpit.p@simformsolutions.com>
2023-04-12 11:52:18 +0530

    Completed Final keyword practical

---
+++ src/com/simformsolutions/finalkeyword/Day.java
@@ -0,0 +1,10 @@
+package com.simformsolutions.finalkeyword;
+
+public class Day {
+    final int minutePerDay = 1440;
+    final int hourPerDay = 24 ;
+    public final void displayHoursAndMinutes(){
+        System.out.println("Number of hours in a day: " + hourPerDay);
+        System.out.println("Number of minutes in a day: " + minutePerDay);
+    }
+}
---
+++ src/com/simformsolutions/finalkeyword/Finalmain.java
@@ -0,0 +1,15 @@
+package com.simformsolutions.finalkeyword;
+
+public class Finalmain extends Day{
+    public static void main(String[] args) {
+        Day Day = new Day();
+//        Day.minutePerDay = 12;
+//        Cannot assign a value to final variable 'minutePerDay'
+
+
+    }
+//    public final void displayHoursAndMinutes(){
+//        System.out.println("hello");
+//    }
+//'displayHoursAndMinutes()' cannot override 'displayHoursAndMinutes()' in 'com.simformsolutions.finalkeyword.Day'; overridden method is final
+}
